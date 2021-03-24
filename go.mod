bd70fd409a8b50eb443e977880f324f972f757f7
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2021-03-24 15:56:23 +0430

    added validator to web service

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	go.mongodb.org/mongo-driver v1.5.0
 	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -3,14 +3,28 @@ package server
 import (
 	"fmt"
 
+	v1 "github.com/Gavazn/Gavazn/server/v1"
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
+type customValidator struct {
+	validator *validator.Validate
+}
+
+// Validate forms
+func (cv *customValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
 // Start web server
 func Start(port string) error {
 	e := echo.New()
+	e.Validator = &customValidator{validator: validator.New()}
 	e.Use(middleware.Logger(), middleware.CORS())
+
+	v1.Register(e)
 
 	return e.Start(fmt.Sprintf(":%v", port))
 }
